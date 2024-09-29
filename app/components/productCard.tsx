7d81087c160b36c37c94b629761e70919bf7c8f8
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-09-29 20:38:11 +0530

    production card ui and ts

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -1,18 +1,22 @@
-// 'use client';
+
 import React from 'react'
 import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
+
 
 const productCard = () => {
   return (
-    <div>
-      {/* <button onClick={() => console.log('Click')}> Add to cart</button> */}
+    <div className={styles.card}>
+      
       <AddToCart />
     </div>
+    
   )
 }
 
 export default productCard
 
+// {/* <button onClick={() => console.log('Click')}> Add to cart</button> */}
 
 // basically this file is server side rendering so if we use this directly or we use click event so it will give error
 // so we need to use ('use client') thi wil convert this file to client side
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,24 @@
+import { link } from 'fs';
 import React from 'react'
 
-const UsersPage = () => {
+interface user {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users',
+    {cache:'no-store'}  // in this line data will get in every 10 min 
+    // here we can use cache function of builtin next.js there are several options like if we put cache: no-store so it wll not store cache like that
+  )
+  const users : user[] = await res.json();
   return (
     <div>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user => <li key = {user.id}>{user.name}</li>)}
+      </ul>
       <p>users page</p>
     </div>
   )
